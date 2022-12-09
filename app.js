e83521e965ba5a9035e8f9cc1e0920d4d88806c8
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-09 10:52:50 +0530

    controller and route setup for login and registration

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+// Register the Routes
+app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
 
---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+exports.register = async (req, res, next) => {
+  res.send("register");
+};
+exports.login = async (req, res, next) => {
+  res.send("login");
+};
