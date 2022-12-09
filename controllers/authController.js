d1bab86fae56cccc4eeee22efb959132ead6a3af
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-09 12:21:06 +0530

    login and registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,58 @@
+const User = require("./../models/user.schema");
+
 exports.register = async (req, res, next) => {
-  res.send("register");
+  try {
+    const { email, username, password } = req.body;
+    const isEmailExist = await User.exists({ email: email.toLowerCase() });
+
+    if (isEmailExist) {
+      return res.status(409).send("Email is alredy Registered with us");
+    }
+
+    const isUsernameExist = await User.exists({
+      username: username.toLowerCase(),
+    });
+
+    if (isUsernameExist) {
+      return res.status(409).send("Username is alredy in use");
+    }
+
+    const user = await User.create({
+      email: email.toLowerCase(),
+      username: username.toLowerCase(),
+      password: password,
+    });
+
+    res.status(200).send({
+      userDetails: {
+        email: user.email,
+        username: user.username,
+        token: user.getJWTToken(),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error occured. Please try Again" + error);
+  }
 };
 exports.login = async (req, res, next) => {
-  res.send("login");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (user && (await user.comparePassword(password))) {
+      return res.status(200).send({
+        userDetails: {
+          email: user.email,
+          username: user.username,
+          token: user.getJWTToken(),
+        },
+      });
+    }
+
+    return res.status(401).send("Invalid Credentials.");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error occured. Please try Again" + error);
+  }
 };
---
+++ models/user.schema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const brcypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await brcypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      email: this.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  return await brcypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
