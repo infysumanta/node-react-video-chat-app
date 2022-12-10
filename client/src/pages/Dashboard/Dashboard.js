c1667f2c7d8f20171e8df7eee10c5d25a3e176f8
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-10 15:55:07 +0530

    Dashboard Login Checking System

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { styled } from "@mui/system";
 import SideBar from "./SideBar/SideBar";
 import FriendsSideBar from "./FriendsSideBar/FriendsSideBar";
 import Messenger from "./Messenger/Messenger";
 import AppBar from "./AppBar/AppBar";
-
+import { logout } from "../../utils/auth";
+import { getActions } from "../../store/actions/authActions";
+import { connect } from "react-redux";
 const Wrapper = styled("div")({
   width: "100%",
   height: "100vh",
   display: "flex",
 });
 
-const Dashboard = () => {
+const Dashboard = ({ setUserDetails }) => {
+  useEffect(() => {
+    const userDetails = localStorage.getItem("user");
+
+    if (!userDetails) {
+      logout();
+    } else {
+      setUserDetails(JSON.parse(userDetails));
+    }
+  });
+
   return (
     <Wrapper>
       <SideBar />
@@ -22,4 +34,10 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch),
+  };
+};
+
+export default connect(null, mapActionsToProps)(Dashboard);
